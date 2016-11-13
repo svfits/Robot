a30a6e220ef7f9bd68a7c71aa5472a5ce31dd746
afanasievdv <afanasievdv@3-510-V7-2>
2016-11-13 15:19:53 +0800

    jjjjj

--- Robot/MainWindow.xaml.cs
+++ Robot/MainWindow.xaml.cs
@@ -4,7 +4,9 @@ using System.Globalization;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,30 +24,91 @@ namespace Robot
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int batteryCharge;
+        System.Timers.Timer timerBatttery = new System.Timers.Timer();
+        System.Timers.Timer timerDateTime = new System.Timers.Timer();
+
         public MainWindow()
         {
             InitializeComponent();
             dateTimeLbl.Content = DateTime.Now.ToString("dd.MM.yyyy HH:mm"); 
-        }      
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {           
-
-        }      
-
+        }   
+        
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
           //  e.Cancel = true;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
+        {           
+            dateTimeUpdate();
+        }
 
+        private void dateTimeUpdate()
+        {
+            timerDateTime.Elapsed += printDateTime;
+            timerDateTime.Interval = 1000;
+            timerDateTime.Start();
+        }
+
+        private void printDateTime(object sender, ElapsedEventArgs e)
+        {           
+            dateTimeLbl.Dispatcher.Invoke(new Action(delegate { dateTimeLbl.Content = DateTime.Now.ToString("dd.MM.yyyy HH:mm"); }));
         }     
 
+        /// <summary>
+        /// зарядим батарейку
+        /// </summary>
+        private void randomBatteryCharge()
+        {
+            Random r = new Random();
+            batteryCharge = r.Next(50,90);
+            
+            timerBatttery.Elapsed += printBatteryCharge;
+            timerBatttery.Interval = 600000;
+            timerBatttery.Start();
+        }
+
+        private void printBatteryCharge(object sender, ElapsedEventArgs e)
+        {
+            batteryCharge++;
+            
+            if(batteryCharge >= 100)
+            {
+                timerBatttery.Stop();
+            }            
+            statusBataryLbl.Dispatcher.Invoke(new Action(delegate { statusBataryLbl.Content = batteryCharge; }));
+        }
+
+        /// <summary>
+        /// при подключении USB
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void connectToUSB_Click(object sender, RoutedEventArgs e)
+        {
+            statusConnectionLbl.Content = "ON";
+            statusConnectionLbl.Foreground = Brushes.Green;
+            modeLbl.Content = "Standby";
+            modeLbl.Foreground = Brushes.Gray;
+            connectBtn.IsEnabled = true;
+        }
+
+        /// <summary>
+        ///  при нажатии кнопки connect
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Connect_Click(object sender, RoutedEventArgs e)
         {
+            randomBatteryCharge();
+            statusBataryLbl.Content = batteryCharge;
+            modeLbl.Content = "Prog";
+            modeLbl.Foreground = Brushes.Green;
 
+            versionProgrammLbl.Foreground = Brushes.Green;
+            versionProgrammLbl.Content = "v.15.7.16";
         }
+
     }
 }
