c6bb7d1c2ed1443dc4919af68c64a484fdf02b5a
afanasievdv <afanasievdv@3-510-V7-2>
2016-12-30 12:57:41 +0800

    добавил тесты

---
+++ UnitTestRobot/HidenExplorer/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robot.HidenExplorer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.HidenExplorer.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void killExplorerTest()
+        {
+            Assert.Fail();
+            
+        }
+    }
+}
